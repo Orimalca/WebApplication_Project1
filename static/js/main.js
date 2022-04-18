fb8f9930bf93d0f773799105c07f1a409aee4044
yuvalkatzir <42411319+yuvalkatzir@users.noreply.github.com>
2022-04-18 16:31:02 +0300

    ezpz

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,7 @@
 "use strict"
 
-import * as ad from "../js/Ad"
-
 const ad_1 = document.getElementById("commercial_1")
 const ads_runningNow = []
 
-function func () {
-    let date = new Date()
-    alert(date.toLocaleDateString() + '\n' + date.toLocaleTimeString())
-}
-
-document.onload = func()
+let date = new Date()
+alert(date.toLocaleDateString() + '\n' + date.toLocaleTimeString())

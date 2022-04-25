b53d83621dbf39bc630647ad2c3018d6380e403e
OmriMosacho <mosacho7@gmail.com>
2022-04-25 20:37:45 +0300

    js changed, jQuery added

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,183 @@
 "use strict"
 
-const ad_1 = document.getElementById("commercial_1")
-const ads_runningNow = []
+$( document ).ready(function() {
+  console.log( "ready!" );
 
-let date = new Date()
-alert(date.toLocaleDateString() + '\n' + date.toLocaleTimeString())
+
+
+class Ad {
+    constructor(name, templateUrl, texts, imagesUrl, days, fromDate, toDate, timeDuration, screens) {
+        this.name = name
+        this.templateUrl = templateUrl
+        this.texts = texts
+        this.imagesUrl = imagesUrl
+        this.days = days
+        this.fromDate = fromDate
+        this.toDate = toDate
+        this.timeDuration = timeDuration
+        this.screens = screens
+    }
+}
+
+
+let ads = [
+    new Ad({
+        name: "one",
+        templateUrl: "templates/templateA.html",
+        texts: ["text1", "text2", "text3", "text4"],
+        imagesUrl: ["images/img1.jpg", "images/img2.jpg"],
+        days: {
+            monday: {
+                fromHour: 6,
+                toHour: 12
+            },
+            wednesday: {
+                fromHour: 13,
+                toHour: 20
+            }
+        },
+
+        fromDate: "1/1/2017",
+        toDate: "12/31/2017",
+        timeDuration: 3,
+        screens: {"1": true, "2": true}
+    }),
+    new Ad({
+        name: "two",
+        templateUrl: "templates/templateB.html",
+        texts: ["text1", "text2", "text3", "text4", "text5", "text6", "text7", "text8", "text9", "text10"],
+        imagesUrl: ["images/img1.jpg"],
+        days: {
+            tuesday: {
+                fromHour: 10,
+                toHour: 16
+            },
+            wednesday: {
+                fromHour: 10,
+                toHour: 16
+            }
+        },
+        fromDate: "3/1/2017",
+        toDate: "4/31/2017",
+        timeDuration: 5,
+        screens: {"1": true, "3": true}
+    }),
+    new Ad({
+        name: "three",
+        templateUrl: "templates/templateC.html",
+        texts: [],
+        imagesUrl: [],
+        days: {
+            all: true,
+            fromHour: 8,
+            toHour: 22
+        },
+        fromDate: "5/1/2017",
+        toDate: "6/15/2017",
+        timeDuration: 7,
+        screens: {"2": true, "3": true}
+    }),
+    new Ad({
+        name: "four",
+        templateUrl: "templates/templateA.html",
+        texts: ["text1", "text2"],
+        imagesUrl: [],
+        days: {
+            monday: {
+                fromHour: 6,
+                toHour: 12
+            }
+        },
+        fromDate: "3/29/2017",
+        toDate: "4/15/2017",
+        timeDuration: 4,
+        screens: {"1": true}
+    }),
+    new Ad({
+        name: "five",
+        templateUrl: "templates/templateB.html",
+        texts: ["text1", "text2", "text3", "text4", "text5", "text6", "text7"],
+        imagesUrl: ["images/img1.jpg", "images/img2.jpg"],
+        days: {
+              monday: {
+                fromHour: 1,
+                toHour: 23,
+            }, tuesday: {
+                fromHour: 1,
+                toHour: 23,
+            }, wednesday: {
+                fromHour: 1,
+                toHour: 23,
+            }
+        },
+
+        fromDate: "4/1/2017",
+        toDate: "4/31/2017",
+        timeDuration: 6,
+        screens: {"3": true}
+    })
+];
+
+//////////////////////////////////////////////
+
+let image = document.getElementById('ad_1');
+
+while(true){
+  
+  let date = new Date();
+  let nowDay_num = date.getDay();
+  let nowDay_str;
+  let nowHour = date.getHours(); 
+
+  switch(nowDay_num) {
+    case 0: nowDay_str = 'sunday'; break;
+    case 1: nowDay_str = 'monday'; break;
+    case 2: nowDay_str = 'tuesday'; break;
+    case 3: nowDay_str = 'wednesday'; break;
+    case 4: nowDay_str = 'thursday'; break;
+    case 5: nowDay_str = 'friday'; break;      
+    case 6: nowDay_str = 'saterday'; break;
+  }
+
+  
+
+  ads.forEach(ad => {
+    let ad_days = ad.days;
+    let ad_duration = ad.timeDuration * 1000;
+    let day_found = false
+    let start_date = new Date(ad.fromDate);
+    let end_date = new Date(ad.toDate);
+
+    if(!(start_date <= date && date <= end_date)) continue;
+
+    days.forEach(day => {
+      if(day.name == nowDay_str && day.fromHour <= nowHour && nowHour <= day.toHour ){
+          
+      }
+    })
+render
+/*
+if (w <=920 && w > 480){
+      setInterval(() => {
+        let random = Math.floor(Math.random() * 3);
+        image.src = commercialsMedium[random].src;
+        p.innerHTML = commercialsMedium[random].text;
+        card.style = "width: 35rem; height: 35rem;"
+      }, 2000);
+    }
+    
+  */
+    
+    
+  });
+
+
+
+
+}
+
+});
+
+
+
+
